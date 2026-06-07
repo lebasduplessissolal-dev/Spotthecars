b3a98abab03a7128d54df74fe7e56f03dc1ae9f7
lebasduplessissolal-dev <lebasduplessissolal@gmail.com>
2026-06-07 14:59:11 +0200

    Create carBrands.js

---
+++ src/data/carBrands.js
@@ -0,0 +1,13 @@
+export const CAR_BRANDS = [
+  "Abarth", "Alfa Romeo", "Alpina", "Alpine", "Aston Martin",
+  "Audi", "Bentley", "BMW", "Brabus", "Bugatti",
+  "Cadillac", "Chevrolet", "Cupra", "Dacia", "Dodge",
+  "Ferrari", "Fiat", "Ford", "Genesis", "Honda",
+  "Hyundai", "Jaguar", "Koenigsegg", "Lamborghini", "Lancia",
+  "Land Rover", "Lexus", "Lotus", "Maserati", "Maybach",
+  "Mazda", "McLaren", "Mercedes-Benz", "Mini", "Mitsubishi",
+  "Morgan", "Nissan", "Pagani", "Peugeot", "Porsche",
+  "Renault", "Rimac", "Rolls-Royce", "Seat", "Skoda",
+  "Smart", "Subaru", "Tesla", "Toyota", "Volkswagen",
+  "Volvo",
+];
